cc4d3acd64857ed2ef6f3fe3245ad28cf8abed64
Chi Man Wong <chiman465@gmail.com>
2021-12-05 07:48:29 +0800

    Update demo_mscca_mstrca_ssvep_recognition.m

--- demo_mscca_mstrca_ssvep_recognition.m
+++ demo_mscca_mstrca_ssvep_recognition.m
@@ -1,22 +1,21 @@
-%% Demo of multi-stimulus CCA or TRCA for SSVEP Recognition %%%
-% In this code, we provide an example of using multi-stimulus canonical
-% correlation analysis (msCCA) and for the ensemble task-related component
-% analysis (eTRCA) SSVEP recognition. Besides, the extended CCA (eCCA) and 
-% the traditional eTRCA are also provided for comparison study.
-% For the above algorithms, please refer the following papers for
-% more details:
-% msCCA: Wong, C. M., et al. (2019). Learning across multi-stimulus enhances target recognition methods in SSVEP-based BCIs. Journal of neural engineering.
-% ms-eTRCA: Wong, C. M., et al. (2019). Learning across multi-stimulus enhances target recognition methods in SSVEP-based BCIs. Journal of neural engineering.
-% eCCA: Chen, X., et al. (2015). High-speed spelling with a noninvasive brain?Vcomputer interface. Proceedings of the national academy of sciences, 112(44), E6058-E6067.
-% eTRCA: Nakanishi, M., et al. (2017). Enhancing detection of SSVEPs for a high-speed brain speller using task-related component analysis. IEEE Transactions on Biomedical Engineering, 65(1), 104-112.
+%This demo shows an example of using multi-stimulus extended canonical correlation analysis (ms-eCCA) and multi-stimulus ensemble task-related component analysis (ms-eTRCA) SSVEP recognition. Besides, the extended CCA (eCCA), the traditional eTRCA, and the task discriminant component analysis (TDCA) are also provided for comparison study.
 
-% In this example, most parameters (such as number of harmonics, time-window lengths and number
-% of neighboring templates) can be adjusted manually to explore their
-% effects on the final performance
-%
+%For the above algorithms, please refer the following papers for more details:
 
-% This code is prepared by Chi Man Wong (chiman465@gmail.com)
-% Date: 16 May 2020
+%ms-eCCA: Wong, C. M., et al. (2019). Learning across multi-stimulus enhances target recognition methods in SSVEP-based BCIs. Journal of neural engineering.
+
+%ms-eTRCA: Wong, C. M., et al. (2019). Learning across multi-stimulus enhances target recognition methods in SSVEP-based BCIs. Journal of neural engineering.
+
+%eCCA: Chen, X., et al. (2015). High-speed spelling with a noninvasive brain-computer interface. Proceedings of the national academy of sciences, 112(44), E6058-E6067.
+
+%eTRCA: Nakanishi, M., et al. (2017). Enhancing detection of SSVEPs for a high-speed brain speller using task-related component analysis. IEEE Transactions on Biomedical Engineering, 65(1), 104-112.
+
+%TDCA: Liu, B., et al. (2021). Improving the Performance of Individually Calibrated SSVEP-BCI by Task-Discriminant Component Analysis. IEEE Transactions on Neural Systems and Rehabilitation Engineering, 29, 1998-2007.
+
+%In this example, most parameters (such as number of harmonics, time-window lengths and number of neighboring templates) can be adjusted manually to explore their effects on the final performance
+
+%This code is prepared by Chi Man Wong (chiman465@gmail.com)
+
 % if you use this code for a publicatiodn, please cite the following papers
 % @article{wong2020learning,
 %   title={Learning across multi-stimulus enhances target recognition methods in SSVEP-based BCIs},
