7197a7b9114c18b7dd91aa5e29b507b545c5f0b0
Chi Man Wong <chiman465@gmail.com>
2022-04-26 17:58:53 +0800

    Update demo_mscca_mstrca_ssvep_recognition.m
    Keep the same data length in preprocessing, training, and testing

--- demo_mscca_mstrca_ssvep_recognition.m
+++ demo_mscca_mstrca_ssvep_recognition.m
@@ -1,21 +1,22 @@
-%This demo shows an example of using multi-stimulus extended canonical correlation analysis (ms-eCCA) and multi-stimulus ensemble task-related component analysis (ms-eTRCA) SSVEP recognition. Besides, the extended CCA (eCCA), the traditional eTRCA, and the task discriminant component analysis (TDCA) are also provided for comparison study.
+%% Demo of multi-stimulus CCA or TRCA for SSVEP Recognition %%%
+% In this code, we provide an example of using multi-stimulus canonical
+% correlation analysis (msCCA) and for the ensemble task-related component
+% analysis (eTRCA) SSVEP recognition. Besides, the extended CCA (eCCA) and
+% the traditional eTRCA are also provided for comparison study.
+% For the above algorithms, please refer the following papers for
+% more details:
+% msCCA: Wong, C. M., et al. (2019). Learning across multi-stimulus enhances target recognition methods in SSVEP-based BCIs. Journal of neural engineering.
+% ms-eTRCA: Wong, C. M., et al. (2019). Learning across multi-stimulus enhances target recognition methods in SSVEP-based BCIs. Journal of neural engineering.
+% eCCA: Chen, X., et al. (2015). High-speed spelling with a noninvasive brain?Vcomputer interface. Proceedings of the national academy of sciences, 112(44), E6058-E6067.
+% eTRCA: Nakanishi, M., et al. (2017). Enhancing detection of SSVEPs for a high-speed brain speller using task-related component analysis. IEEE Transactions on Biomedical Engineering, 65(1), 104-112.
 
-%For the above algorithms, please refer the following papers for more details:
+% In this example, most parameters (such as number of harmonics, time-window lengths and number
+% of neighboring templates) can be adjusted manually to explore their
+% effects on the final performance
+%
 
-%ms-eCCA: Wong, C. M., et al. (2019). Learning across multi-stimulus enhances target recognition methods in SSVEP-based BCIs. Journal of neural engineering.
-
-%ms-eTRCA: Wong, C. M., et al. (2019). Learning across multi-stimulus enhances target recognition methods in SSVEP-based BCIs. Journal of neural engineering.
-
-%eCCA: Chen, X., et al. (2015). High-speed spelling with a noninvasive brain-computer interface. Proceedings of the national academy of sciences, 112(44), E6058-E6067.
-
-%eTRCA: Nakanishi, M., et al. (2017). Enhancing detection of SSVEPs for a high-speed brain speller using task-related component analysis. IEEE Transactions on Biomedical Engineering, 65(1), 104-112.
-
-%TDCA: Liu, B., et al. (2021). Improving the Performance of Individually Calibrated SSVEP-BCI by Task-Discriminant Component Analysis. IEEE Transactions on Neural Systems and Rehabilitation Engineering, 29, 1998-2007.
-
-%In this example, most parameters (such as number of harmonics, time-window lengths and number of neighboring templates) can be adjusted manually to explore their effects on the final performance
-
-%This code is prepared by Chi Man Wong (chiman465@gmail.com)
-
+% This code is prepared by Chi Man Wong (chiman465@gmail.com)
+% Date: 16 May 2020
 % if you use this code for a publicatiodn, please cite the following papers
 % @article{wong2020learning,
 %   title={Learning across multi-stimulus enhances target recognition methods in SSVEP-based BCIs},
@@ -41,7 +42,7 @@ close all;
 % Wang, Y., et al. (2016). A benchmark dataset for SSVEP-based brain-computer interfaces. IEEE Transactions on Neural Systems and Rehabilitation Engineering, 25(10), 1746-1752.
 % Then indicate where the directory of the dataset is :
 
-% addpath('..\mytoolbox\');
+addpath('..\mytoolbox\');
 Fs=250; % sample rate
 
 dataset_no=1;
@@ -59,14 +60,14 @@ if dataset_no==1
     sti_f=[8.0:1:15.0, 8.2:1:15.2,8.4:1:15.4,8.6:1:15.6,8.8:1:15.8];
     n_sti=length(sti_f);                     % number of stimulus frequencies
     [~,target_order]=sort(sti_f);
-    sti_f=sti_f(target_order);   
+    sti_f=sti_f(target_order);
 elseif dataset_no==2
     str_dir='..\BETA SSVEP dataset\';
     num_of_wn=4;                        % for TDCA
     num_of_k=9;                         % for TDCA
     num_of_delay=4;                     % for TDCA
     latencyDelay = round(0.13*Fs);      % latency
-    num_of_subj=70; 
+    num_of_subj=70;
     ch_used=[48 54 55 56 57 58 61 62 63]; % Pz, PO5, PO3, POz, PO4, PO6, O1,Oz, O2 (in SSVEP benchmark dataset)
     num_of_trials=3;                    % Number of training trials (1<=num_of_trials<=3)
     pha_val=[0 0.5 1 1.5 0 0.5 1 1.5 0 0.5 1 1.5 0 0.5 1 1.5 0 0.5 1 1.5 ...
@@ -74,20 +75,9 @@ elseif dataset_no==2
     sti_f=[8.6:0.2:15.8,8.0 8.2 8.4];
     n_sti=length(sti_f);                     % number of stimulus frequencies
     [~,target_order]=sort(sti_f);
-    sti_f=sti_f(target_order);    
+    sti_f=sti_f(target_order);
 else
-    str_dir=cd;                         % exampleData.mat
-    ch_used=[1:9];
-    num_of_trials=2;                    % Number of training trials (1<=num_of_trials<=2)
-    pha_val=[0 0.5 1 1.5 0 0.5 1 1.5 0 0.5 1 1.5 0 0.5 1 1.5 0 0.5 1 1.5 ...
-        0 0.5 1 1.5 0 0.5 1 1.5 0 0.5 1 1.5 0 0.5 1 1.5 0 0.5 1 1.5]*pi;
-    sti_f=[8:0.2:15.8];
-    n_sti=length(sti_f);                     % number of stimulus frequencies
-    temp=reshape([1:40],8,5);
-    temp=temp';
-    target_order=temp(:)';  
 end
-
 
 
 num_of_harmonics=5;                 % for all cca-based methods
@@ -97,23 +87,26 @@ num_of_r=4;                         % for ecca
 num_of_subbands=5;                  % for filter bank analysis
 FB_coef0=[1:num_of_subbands].^(-1.25)+0.25; % for filter bank analysis
 
+
 % About the above parameter, please check the related paper:
 % Chen, X., et al. (2015). Filter bank canonical correlation analysis for implementing a high-speed SSVEP-based brain?Vcomputer interface. Journal of neural engineering, 12(4), 046008.
 
 % time-window length (min_length:delta_t:max_length)
-min_length=0.4;
+min_length=0.3;
 delta_t=0.1;
-max_length=0.4;                     % [min_length:delta_t:max_length]
+max_length=0.3;                     % [min_length:delta_t:max_length]
 
+TW = min_length:delta_t:max_length;
+TW_p = round(TW*Fs);
 enable_bit=[1 1 1 1 1];             % Select the algorithms: bit 1: eCCA, bit 2: ms-eCCA, bit 3: eTRCA, bit 4: ms-eTRCA, bit 5: TDCA, e.g., enable_bit=[1 1 1 1 1]; -> select all four algorithms
-is_center_std=0;                    % 0: without , 1: with (zero mean, and unity standard deviation)
+is_center_std=1;                    % 0: without , 1: with (zero mean, and unity standard deviation)
 
 % Chebyshev Type I filter design
 for k=1:num_of_subbands
     Wp = [(8*k)/(Fs/2) 90/(Fs/2)];
     Ws = [(8*k-2)/(Fs/2) 100/(Fs/2)];
     [N,Wn] = cheb1ord(Wp,Ws,3,40);
-    [subband_signal(k).bpB,subband_signal(k).bpA] = cheby1(N,0.5,Wn);    
+    [subband_signal(k).bpB,subband_signal(k).bpA] = cheby1(N,0.5,Wn);
 end
 %notch
 Fo = 50;
@@ -122,121 +115,119 @@ BW = (Fo/(Fs/2))/Q;
 
 [notchB,notchA] = iircomb(Fs/Fo,BW,'notch');
 seed = RandStream('mt19937ar','Seed','shuffle');
-for sn=1:num_of_subj
-    tic    
-    if dataset_no==1
-        load(strcat(str_dir,'S',num2str(sn),'.mat'));
-    elseif dataset_no==2
-        load([str_dir 'S' num2str(sn) '.mat']);
-        eegdata=data.EEG;
-        data = permute(eegdata,[1 2 4 3]);
-    else
-        load(strcat(str_dir,'\','exampleData.mat'));        
-    end
+
+for tw_length=1:length(TW)
+    sig_len=TW_p(tw_length);
     
-    %  pre-stimulus period: 0.5 sec
-    %  latency period: 0.14 sec    
-    eeg=data(ch_used,floor(0.5*Fs)+1:floor(0.5*Fs+latencyDelay)+2*Fs,:,:);
-    
-    [d1_,d2_,d3_,d4_]=size(eeg);
-    d1=d3_;d2=d4_;d3=d1_;d4=d2_;
-    no_of_class=d1;
-    % d1: num of stimuli
-    % d2: num of trials
-    % d3: num of channels % Pz, PO5, PO3, POz, PO4, PO6, O1, Oz, O2
-    % d4: num of sampling points
-    for i=1:1:d1
-        for j=1:1:d2
-            y0=reshape(eeg(:,:,i,j),d3,d4);
-            SSVEPdata(:,:,j,i)=reshape(y0,d3,d4,1,1);
-            y = filtfilt(notchB, notchA, y0.'); %notch
-            y = y.';
-            for sub_band=1:num_of_subbands
-                
-                for ch_no=1:d3                    
-                    tmp2=filtfilt(subband_signal(sub_band).bpB,subband_signal(sub_band).bpA,y(ch_no,:));
-                    y_sb(ch_no,:) = tmp2(latencyDelay+1:latencyDelay+2*Fs);
-                end
-                
-                subband_signal(sub_band).SSVEPdata(:,:,j,i)=reshape(y_sb,d3,length(y_sb),1,1);
-            end
-            
-        end
-    end
-    
-    clear eeg
-    %% Initialization
-    
-    n_ch=size(SSVEPdata,1);
-    
-    TW=min_length:delta_t:max_length;
-    TW_p=round(TW*Fs);
-    n_run=d2;                                % number of used runs
-        
-    SSVEPdata=SSVEPdata(:,:,:,target_order);
-    for sub_band=1:num_of_subbands
-        subband_signal(sub_band).SSVEPdata=subband_signal(sub_band).SSVEPdata(:,:,:,target_order); % To sort the orders of the data as 8.0, 8.2, 8.4, ..., 15.8 Hz
-    end
-    
-    
-    FB_coef=FB_coef0'*ones(1,n_sti);
-    n_correct=zeros(length(TW),7); % Count how many correct detection
-    
-    
-    seq_0=zeros(d2,num_of_trials);
-    for run=1:d2
-        %         % leave-one-run-out cross-validation        
-        if (num_of_trials==1)
-            seq1=run;
-        elseif (num_of_trials==d2-1)
-            seq1=[1:n_run];
-            seq1(run)=[];
+    for sn=1:num_of_subj
+        tic
+        if dataset_no==1
+            load(strcat(str_dir,'S',num2str(sn),'.mat'));
+        elseif dataset_no==2
+            load([str_dir 'S' num2str(sn) '.mat']);
+            eegdata=data.EEG;
+            data = permute(eegdata,[1 2 4 3]);
         else
-            % leave-one-run-out cross-validation
-            % Randomly select the trials for training
-            isOK=0;
-            while (isOK==0)
-                seq=randperm(seed,d2);
-                seq1=seq(1:num_of_trials);
-                seq1=sort(seq1);
-                if isempty(find(sum((seq1'*ones(1,d2)-seq_0').^2)==0))
-                    isOK=1;
-                end
-            end
-            
+%             load(strcat(str_dir,'\','exampleData.mat'));
         end
-        idx_traindata=seq1; % index of the training trials
-        idx_testdata=1:n_run; % index of the testing trials
-        idx_testdata(seq1)=[];
         
-        for i=1:no_of_class
-            if length(idx_traindata)>1
-                signal_template(i,:,:)=mean(SSVEPdata(:,:,idx_traindata,i),3);
+        %  pre-stimulus period: 0.5 sec
+        %  latency period: 0.14 sec
+        eeg=data(ch_used,floor(0.5*Fs)+1:floor(0.5*Fs+latencyDelay)+sig_len,:,:);
+        
+        [d1_,d2_,d3_,d4_]=size(eeg);
+        d1=d3_;d2=d4_;d3=d1_;d4=d2_;
+        no_of_class=d1;
+        % d1: num of stimuli
+        % d2: num of trials
+        % d3: num of channels % Pz, PO5, PO3, POz, PO4, PO6, O1, Oz, O2
+        % d4: num of sampling points
+        for i=1:1:d1
+            for j=1:1:d2
+                y0=reshape(eeg(:,:,i,j),d3,d4);
+                SSVEPdata(:,:,j,i)=reshape(y0,d3,d4,1,1);
+                y = filtfilt(notchB, notchA, y0.'); %notch
+                y = y.';
+                for sub_band=1:num_of_subbands
+                    
+                    for ch_no=1:d3
+                        tmp2=filtfilt(subband_signal(sub_band).bpB,subband_signal(sub_band).bpA,y(ch_no,:));
+                        y_sb(ch_no,:) = tmp2(latencyDelay+1:latencyDelay+sig_len);
+                    end
+                    
+                    subband_signal(sub_band).SSVEPdata(:,:,j,i)=reshape(y_sb,d3,length(y_sb),1,1);
+                end
+                
+            end
+        end
+        
+        clear eeg
+        %% Initialization
+        n_ch=size(SSVEPdata,1);
+        n_run=d2;                                % number of used runs
+        
+        SSVEPdata=SSVEPdata(:,:,:,target_order);
+        for sub_band=1:num_of_subbands
+            subband_signal(sub_band).SSVEPdata=subband_signal(sub_band).SSVEPdata(:,:,:,target_order); % To sort the orders of the data as 8.0, 8.2, 8.4, ..., 15.8 Hz
+        end        
+        
+        FB_coef=FB_coef0'*ones(1,n_sti);
+        n_correct=zeros(1,7); % Count how many correct detection        
+        
+        seq_0=zeros(d2,num_of_trials);
+        for run=1:d2
+            %         % leave-one-run-out cross-validation
+            if (num_of_trials==1)
+                seq1=run;
+            elseif (num_of_trials==d2-1)
+                seq1=[1:n_run];
+                seq1(run)=[];
             else
-                signal_template(i,:,:)=SSVEPdata(:,:,idx_traindata,i);
-            end
-            for k=1:num_of_subbands
-                if length(idx_traindata)>1
-                    subband_signal(k).signal_template(i,:,:)=mean(subband_signal(k).SSVEPdata(:,:,idx_traindata,i),3);
-                else
-                    subband_signal(k).signal_template(i,:,:)=subband_signal(k).SSVEPdata(:,:,idx_traindata,i);
+                % leave-one-run-out cross-validation
+                % Randomly select the trials for training
+                isOK=0;
+                while (isOK==0)
+                    seq=randperm(seed,d2);
+                    seq1=seq(1:num_of_trials);
+                    seq1=sort(seq1);
+                    if isempty(find(sum((seq1'*ones(1,d2)-seq_0').^2)==0))
+                        isOK=1;
+                    end
                 end
+                
+            end
+            idx_traindata=seq1; % index of the training trials
+            idx_testdata=1:n_run; % index of the testing trials
+            idx_testdata(seq1)=[];
+            
+            for i=1:no_of_class
+                if length(idx_traindata)>1
+                    signal_template(i,:,:)=mean(SSVEPdata(:,:,idx_traindata,i),3);
+                else
+                    signal_template(i,:,:)=SSVEPdata(:,:,idx_traindata,i);
+                end
+                for k=1:num_of_subbands
+                    if length(idx_traindata)>1
+                        subband_signal(k).signal_template(i,:,:)=mean(subband_signal(k).SSVEPdata(:,:,idx_traindata,i),3);
+                    else
+                        subband_signal(k).signal_template(i,:,:)=subband_signal(k).SSVEPdata(:,:,idx_traindata,i);
+                    end
+                end
+                
+                
             end
             
             
-        end
-        
-        
-        for run_test=1:length(idx_testdata)
-            for tw_length=1:length(TW)
+            for run_test=1:length(idx_testdata)
+                %             for tw_length=1:length(TW)
                 clear Xa Xa_train
-                sig_len=TW_p(tw_length);
+                %                 sig_len=TW_p(tw_length);
                 test_signal=zeros(d3,sig_len);
                 fprintf('Testing TW %fs, No.crossvalidation %d \n',TW(tw_length),idx_testdata(run_test));
                 
                 % TDCA training
                 if enable_bit(5)==1
-                    for sub_band=1:num_of_subbands                        
+                    for sub_band=1:num_of_subbands
                         for j=1:no_of_class
                             Ref=ref_signal_nh(sti_f(j),Fs,0,sig_len,num_of_harmonics);
                             [Q_ref1,R_ref1]=qr(Ref',0);
@@ -244,12 +235,12 @@ for sn=1:num_of_subj
                             for train_no=1:length(idx_traindata)
                                 traindata_1a=[];
                                 for dn=1:num_of_delay
-                                    traindata=reshape(subband_signal(sub_band).SSVEPdata(:,[dn:sig_len+dn-1],idx_traindata(train_no),j),d3,sig_len);
+                                    traindata=reshape(subband_signal(sub_band).SSVEPdata(:,[dn:sig_len],idx_traindata(train_no),j),d3,sig_len-dn+1);
                                     if (is_center_std==1)
                                         traindata=traindata-mean(traindata,2)*ones(1,length(traindata));
                                         traindata=traindata./(std(traindata')'*ones(1,length(traindata)));
                                     end
-                                    traindata_1a=[traindata_1a;traindata];
+                                    traindata_1a=[traindata_1a;[traindata zeros(length(ch_used),dn-1)]];
                                 end
                                 traindata_1a_P=traindata_1a*P;
                                 if (is_center_std==1)
@@ -498,55 +489,52 @@ for sn=1:num_of_subj
                     
                     [~,idx]=max(CCAR1);
                     if idx==i
-                        n_correct(tw_length,1)=n_correct(tw_length,1)+1;
+                        n_correct(1)=n_correct(1)+1;
                     end
                     [~,idx]=max(CR1);
                     if idx==i
-                        n_correct(tw_length,2)=n_correct(tw_length,2)+1;
+                        n_correct(2)=n_correct(2)+1;
                     end
                     [~,idx]=max(msccaR1);
                     if idx==i
-                        n_correct(tw_length,3)=n_correct(tw_length,3)+1;
+                        n_correct(3)=n_correct(3)+1;
                     end
                     [~,idx]=max(TRCAR1);
                     if idx==i
-                        n_correct(tw_length,4)=n_correct(tw_length,4)+1;
+                        n_correct(4)=n_correct(4)+1;
                     end
                     [~,idx]=max(MSTRCAR1);
                     if idx==i
-                        n_correct(tw_length,5)=n_correct(tw_length,5)+1;
+                        n_correct(5)=n_correct(5)+1;
                     end
                     [~,idx]=max(MSCCATRCAR1);
                     if idx==i
-                        n_correct(tw_length,6)=n_correct(tw_length,6)+1;
+                        n_correct(6)=n_correct(6)+1;
                     end
                     [~,idx]=max(TDCAR1);
                     if idx==i
-                        n_correct(tw_length,7)=n_correct(tw_length,7)+1;
+                        n_correct(7)=n_correct(7)+1;
                     end
                 end
+                %             end
             end
+            idx_train_run(run,:)=idx_traindata;
+            idx_test_run(run,:)=idx_testdata;
+            seq_0(run,:)=seq1;
         end
-        idx_train_run(run,:)=idx_traindata;
-        idx_test_run(run,:)=idx_testdata;
-        seq_0(run,:)=seq1;
+        
+        
+        %% Save results
+        toc
+        accuracy=100*n_correct/n_sti/n_run/length(idx_testdata)
+        all_sub_acc(tw_length,sn,:)=accuracy;
+        % column 1: CCA
+        % column 2: eCCA
+        % column 3: ms-eCCA
+        % column 4: eTRCA
+        % column 5: ms-eTRCA
+        % column 6: ms-eCCA + ms-eTRCA
+        % column 7: TDCA
+        disp(sn)
     end
-    
-    
-    %% Save results
-    toc
-    accuracy=100*n_correct/n_sti/n_run/length(idx_testdata)
-    % column 1: CCA
-    % column 2: eCCA
-    % column 3: ms-eCCA
-    % column 4: eTRCA
-    % column 5: ms-eTRCA
-    % column 6: ms-eCCA + ms-eTRCA
-    % column 7: TDCA
-    if dataset_no==1
-        xlswrite('acc_file.xlsx',accuracy'/100,strcat('Sheet',num2str(sn)));
-    else
-        xlswrite('acc_file2.xlsx',accuracy'/100,strcat('Sheet',num2str(sn)));
-    end
-    disp(sn)
 end
