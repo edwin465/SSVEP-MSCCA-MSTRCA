eeefc16da09bf5dad539465c2c62075e4a41d7e0
Chi Man Wong <chiman465@gmail.com>
2022-04-28 00:06:14 +0800

    Add files via upload

--- plot_results.m
+++ plot_results.m
@@ -30,9 +30,10 @@ legend_str={'eCCA','ms-eCCA','eTRCA','ms-eTRCA','ms-eCCA+ms-eTRCA','TDCA'};
 x_tick_str={'0.3','0.4','0.5','0.6','0.7','0.8','0.9','1.0'};  
 
 figure(1);
+% fun_barchart_errorbar_plot.m (https://github.com/edwin465/matlab-mytoolbox-plot)
 fun_barchart_errorbar_plot(data,num_of_method,num_of_tw,color_rgb,title_str,x_lab_str,y_lab_str,legend_str(1:num_of_method),x_tick_str);
 
-dat_x=[1:num_of_tw];
+dat_x=[0.3:0.1:1.0];
 dat_y1=squeeze(all_sub_acc(:,:,6));
 dat_y1=dat_y1';
 dat_y2=squeeze(all_sub_acc(:,:,7));
@@ -42,6 +43,7 @@ x_lab_str='Tw (s)';
 y_lab_str='Accuracy (%)';
 legend_str1='ms-eCCA+ms-eTRCA';legend_str2='TDCA';
 test_pval0=1; % Calculate the paired t-test result (tail: both))
+% fun_errorbarchart.m (https://github.com/edwin465/matlab-mytoolbox-plot)
 fun_errorbarchart(dat_x,dat_y1,dat_y2,test_pval0,title_str,x_lab_str,y_lab_str,legend_str1,legend_str2);
 
 
